8bf052f4fadb05f9dd0b995abf5a659343292566
Mateusz <ganski213@gmail.com>
2024-02-25 21:59:48 +0100

    added tic_tac_toe directory

---
+++ tic_tac_toe/README.md
@@ -0,0 +1,36 @@
+tic_tac_toe.py - documentation
+
+This program is an implementation of tic tac toe game, where user is playing against program.
+
+How does this program work?
+
+Firstly, this program asks user which sign (x or o) they want to play as, define choosen sign as a user's sign and another sign as a program's sign. Then, as long as none of the sides wins and there still are free fields on the table, program repeatedly asks user about coordinates of table field where they want to put their sign and then, after user placed their sign on a choosen field, program chooses the place where it wants to put their sign. Game ends when user or program will put three of their signs on a table in a row or in a slant (then either user wins or program wins, if both user and program will manage to do that in the same turn, game will end with a draw), or if all the fields on the table will be occupied before user or program will win.
+
+How to use this program?
+
+In order to use this program, user simply has to type 'python3 tic_tac_toe.py' into the terminal while being in directory where this program is located on user's device. After this step, user has to choose a sign they want to use during the game and then, with every turn, user has to choose coordinates of the table field where they want to put their sign.
+
+Description of all program's variables:
+
+| variable name | variable description |
+| ------------- | -------------------- |
+| table | 3x3 table where user and program will put their signs during every turn |
+| userSign | String, stores the sign choosen by the user (its value can be either "x" or "o") |
+| programSignValues | Dictionary, it's used to define value of programSign based on the value of userSign |
+| programSign | String, stores the sign that will be used by program during the game (if userSign = "x", it's equal to "o", if userSign = "o", it's equal to "x") |
+| userMove | String, stores coordinates of the field where user wants to put their sign in certain turn, it's written in the following format - rc (r means number of the row, c means number of the column) |
+| programMoveRow | Integer, defines number of the row of the table field where program will put its sign in a certain turn, its value is choosen randomly |
+| programMoveColumn | Integer, defines number of the column of the table field where program will put its sign in a certain turn, its value is choosen randomly |
+| userVictory | Integer, its value determines whether user managed to put three of their signs into a table in a row or in a slant (if its value is 1 - yes, otherwise - no) |
+| programVictory | Integer, its value determines whether program managed to put three of its signs into a table in a row or in a slant (if its value is 1 - yes, otherwise - no) |
+| occupatedFields | Integer, stores the amount of occupated fields on the table |
+
+Description of all program's functions:
+
+| function name | function description |
+| ------------- | -------------------- |
+| printTable | This function displays current state of the table, with every sign that is present on it at the moment |
+| takeUserSign | This function takes the sign which user wants to use during the game |
+| takeUserMove | This function takes coordinates of the field where user wants to place their sign during the certain turn |
+| takeProgramMove | This function takes coordinates of the field where program wants to place its sign during the certain turn |
+| checkVictory | This function checks if any of the players (user and/or program) managed to put three of their signs into the table in a row or in a slant and changes values of userVictory and programVictory depending on the result |
---
+++ tic_tac_toe/tic_tac_toe.py
@@ -0,0 +1,86 @@
+import random
+
+def printTable(table):
+    print("{}|{}|{}\n{}|{}|{}\n{}|{}|{}\n".format(table[0][0],table[0][1],table[0][2],table[1][0],table[1][1],table[1][2],table[2][0],table[2][1],table[2][2]))
+    
+def takeUserSign():
+    userSign = str(input("choose sign you want to use (x or o): "))
+    programSignValues = {"x": "o", "o": "x"}
+    programSign = programSignValues[userSign]
+    return userSign, programSign
+
+def takeUserMove(table):
+    userMove = str(input("your turn - insert coordinates of the field where you want to put your number (in a following order - rc, where r means number of row and c means number of column): "))
+    while not table[int(userMove[0])-1][int(userMove[1])-1] == ' ':
+        print("player chose field that's already occupated")
+        userMove = str(input("your turn - insert coordinates of the field where you want to put your number (in a following order - rc, where r means number of row and c means number of column): "))
+    if len(userMove) > 2:
+        raise ValueError
+    return userMove
+
+def takeProgramMove(table):
+    programMoveRow = random.randint(1,3)
+    programMoveColumn = random.randint(1,3)
+    while table[programMoveRow-1][programMoveColumn-1] == "x" or table[programMoveRow-1][programMoveColumn-1] == "o":
+        programMoveRow = random.randint(1,3)
+        programMoveColumn = random.randint(1,3)
+    return programMoveRow, programMoveColumn
+
+def checkVictory(table, userSign, programSign):
+    if table[0][0] == table[0][1] == table[0][2] == userSign or table[1][0] == table[1][1] == table[1][2] == userSign or table[2][0] == table[2][1] == table[2][2] == userSign or table[0][0] == table[1][1] == table[2][2] == userSign or table[0][2] == table[1][1] == table[2][0] == userSign or table[0][0] == table[1][0] == table[2][0] == userSign or table[0][1] == table[1][1] == table[2][1] == userSign or table[0][2] == table[1][2] == table[2][2] == userSign:
+        userVictory = 1
+    if table[0][0] == table[0][1] == table[0][2] == programSign or table[1][0] == table[1][1] == table[1][2] == programSign or table[2][0] == table[2][1] == table[2][2] == programSign or table[0][0] == table[1][1] == table[2][2] == programSign or table[0][2] == table[1][1] == table[2][0] == programSign or table[0][0] == table[1][0] == table[2][0] == programSign or table[0][1] == table[1][1] == table[2][1] == programSign or table[0][2] == table[1][2] == table[2][2] == programSign:
+        programVictory = 1
+    return userVictory, programVictory
+    
+def main():
+    try:
+        table = [
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']
+        ]
+        occupatedFields = 0
+        userVictory = 0
+        programVictory = 0
+        userSign, programSign = takeUserSign()
+        while userVictory+programVictory == 0:
+            userMove = takeUserMove(table)
+            table[int(userMove[0])-1][int(userMove[1])-1] = userSign
+            printTable(table)
+            occupatedFields = occupatedFields+1
+            if occupatedFields == 9:
+                print("the end")
+                break
+            programMoveRow, programMoveColumn = takeProgramMove(table)
+            table[programMoveRow-1][programMoveColumn-1] = programSign
+            print("program's move:")
+            printTable(table)
+            occupatedFields = occupatedFields+1
+            if occupatedFields == 9:
+                print("the end")
+                break
+            userVictory, programVictory = checkVictory(table, userSign, programSign)
+            if userVictory+programVictory == 1:
+                if userVictory == 1:
+                    print("you win")
+                    break
+                elif programVictory == 1:
+                    print("program wins")
+                    break
+                else:
+                    print("something went wrong")
+                    break
+            if userVictory+programVictory == 2:
+                print("draw")
+                break
+            if occupatedFields > 9:
+                print("the end")
+                break
+    except (ValueError, IndexError):
+        print("Error: user chose invalid coordinates of table field")
+    except KeyError:
+        print("Error: user chose invalid sign")
+
+if __name__ == "__main__":
+    main()	
