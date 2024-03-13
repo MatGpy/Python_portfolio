73d79458e62f0fdfb42e5ea02cea1c23aa093039
Mateusz <ganski213@gmail.com>
2024-03-13 22:55:49 +0100

    removed some unnecessary parameters of takeUserDate function

--- loan_installments_calculator/loan_installments_calculator.py
+++ loan_installments_calculator/loan_installments_calculator.py
@@ -1,12 +1,12 @@
 from rich.table import Table
 from rich import print
 
-def takeUserDate(daysReversed, monthsReversed, loanAmount, loanCommission, loanInterestRate, periodOfPayment):
+def takeUserDate(daysReversed, monthsReversed):
     userDate = str(input("Insert the date when your loan was taken (in dd-mm-yyyy format): "))
     userDay = userDate[:2]
     userMonth = userDate[3:5]
     userYear = userDate[6:]
-    if userDay not in daysReversed or userMonth not in monthsReversed or int(userYear) <= 0 or loanAmount <= 0 or loanCommission <= 0 or loanInterestRate <= 0 or periodOfPayment <= 0:
+    if userDay not in daysReversed or userMonth not in monthsReversed or int(userYear) <= 0:
         raise ValueError
     return userDay, userMonth, userYear
 
@@ -19,15 +19,20 @@ def takeLoanAmount():
 def takeLoanCommission():
     loanCommission = int(input("Insert your bank's loan commission (in %): "))
     loanCommission = loanCommission/100
+    if loanCommission <= 0:
+        raise ValueError
     return loanCommission
 
 def takeLoanInterestRate():
     loanInterestRate = int(input("Insert your loan's annual interest rate (in %): "))
+    loanInterestRate = loanInterestRate/100
+    if loanInterestRate <= 0:
+        raise ValueError
     return loanInterestRate
 
 def takePeriodOfPayment():
     periodOfPayment = int(input("Insert period of your loan's payment (in months): "))
-    if periodOfPayment > 240:
+    if periodOfPayment > 240 or periodOfPayment <= 0:
         raise TypeError
     return periodOfPayment
 
@@ -80,19 +85,18 @@ def main():
         days = {1: "01", 2: "02", 3: "03", 4: "04", 5: "05", 6: "06", 7: "07", 8: "08", 9: "09", 10: "10", 11: "11", 12: "12", 13: "13", 14: "14", 15: "15", 16: "16", 17: "17", 18: "18", 19: "19", 20: "20", 21: "21", 22: "22", 23: "23", 24: "24", 25: "25", 26: "26", 27: "27", 28: "28", 29: "29", 30: "30", 31: "31"}
         daysReversed = {v: k for k, v in days.items()}
         wiborRates = {1: 7.00, 2: 7.00, 3: 6.95, 4: 6.95, 5: 6.95, 6: 6.95, 7: 6.70, 8: 6.55, 9: 5.65, 10: 5.60, 11: 5.80, 12: 5.65}
-        userDay, userMonth, userYear = takeUserDate(daysReversed, monthsReversed, loanAmount, loanCommission, loanInterestRate, periodOfPayment)
+        userDay, userMonth, userYear = takeUserDate(daysReversed, monthsReversed)
         loanAmount = takeLoanAmount()
         loanCommission = takeLoanCommission()
         loanInterestRate = takeLoanInterestRate()
         periodOfPayment = takePeriodOfPayment()
         totalLoanAmount = calculateTotalLoan(loanAmount, loanCommission)
-        loanInterestRate = loanInterestRate/100
         installment = calculateAverage(periodOfPayment, loanInterestRate, totalLoanAmount, wiborRates, monthsReversed[userMonth])
         createTable(periodOfPayment, days, months, daysReversed[userDay], monthsReversed[userMonth], int(userYear), installment)
     except ValueError:
         print("Error: incorrect data in the input.")
     except TypeError:
-        print("Error, amount of your loan can't be lower than 1000 PLN and period of your loan's payment can't be longer than 20 years.")
+        print("Error, amount of your loan can't be lower than 1000 PLN and period of your loan's payment can't be longer than 20 years or shorter than 0 months.")
 
 if __name__ == "__main__":
     main()
